b2f5fa328ee5465d0d1c0cc4c82804ce451f96fa
weiyaliu <343445839@tdsb.ca>
2026-03-11 16:03:44 +0000

    sacjms

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,28 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean isEven (int n) {
+		boolean even = n%2 == 0;
+		return even;
+	}
+
+	public static boolean isOdd(int n){
+		boolean odd = n%2 != 0;
+		return odd;
+	}
+
+	public static boolean isPositive(int n){
+		boolean pos = n>0;
+		return pos;
+	}
+
+	public static boolean isNegative(int n){
+		boolean neg = n<0;
+		return neg;
+	}
+
+	public static int combinedLength(String a, String b) {
+		String ab = a + " " + b;
+		return ab;
+	}
 }
